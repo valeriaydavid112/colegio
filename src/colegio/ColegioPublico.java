37b500c136f9c03e8573b44bcdcc64fa51c22a12
valeriaydavid112 <51164843+valeriaydavid112@users.noreply.github.com>
2019-10-02 13:07:38 -0500

    version 1.3

--- src/colegio/ColegioPublico.java
+++ src/colegio/ColegioPublico.java
@@ -4,6 +4,10 @@ package colegio;
 public class ColegioPublico extends Colegio{
     
     private int personalTecnico;
+    private date fechaDeFundacion;
+    private string nombreDelRector;
+    private boolean sueldoDeProfesores;
+    
     
     public ColegioPublico(int numeroDeEstudiantes, Boolean mixto, String nombre) {
         super(numeroDeEstudiantes, mixto, nombre);
