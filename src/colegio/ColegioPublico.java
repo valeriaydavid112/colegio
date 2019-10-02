36cbc44f72c21fa8111087d3c837e2a9faba9f4c
valeriaydavid112 <51164843+valeriaydavid112@users.noreply.github.com>
2019-10-02 13:10:19 -0500

    version1.4

--- src/colegio/ColegioPublico.java
+++ src/colegio/ColegioPublico.java
@@ -12,5 +12,45 @@ public class ColegioPublico extends Colegio{
     public ColegioPublico(int numeroDeEstudiantes, Boolean mixto, String nombre) {
         super(numeroDeEstudiantes, mixto, nombre);
     }
+
+    public ColegioPublico(int personalTecnico, date fechaDeFundacion, string nombreDelRector, boolean sueldoDeProfesores, int numeroDeEstudiantes, Boolean mixto, String nombre) {
+        super(numeroDeEstudiantes, mixto, nombre);
+        this.personalTecnico = personalTecnico;
+        this.fechaDeFundacion = fechaDeFundacion;
+        this.nombreDelRector = nombreDelRector;
+        this.sueldoDeProfesores = sueldoDeProfesores;
+    }
+
+    public int getPersonalTecnico() {
+        return personalTecnico;
+    }
+
+    public date getFechaDeFundacion() {
+        return fechaDeFundacion;
+    }
+
+    public string getNombreDelRector() {
+        return nombreDelRector;
+    }
+
+    public boolean isSueldoDeProfesores() {
+        return sueldoDeProfesores;
+    }
+
+    public void setPersonalTecnico(int personalTecnico) {
+        this.personalTecnico = personalTecnico;
+    }
+
+    public void setFechaDeFundacion(date fechaDeFundacion) {
+        this.fechaDeFundacion = fechaDeFundacion;
+    }
+
+    public void setNombreDelRector(string nombreDelRector) {
+        this.nombreDelRector = nombreDelRector;
+    }
+
+    public void setSueldoDeProfesores(boolean sueldoDeProfesores) {
+        this.sueldoDeProfesores = sueldoDeProfesores;
+    }
     
 }
