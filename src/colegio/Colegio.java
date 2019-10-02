150d2459c6d01f83112e0b6ca0fe64dbd508c0a7
valeriaydavid112 <51164843+valeriaydavid112@users.noreply.github.com>
2019-10-02 12:57:33 -0500

    Version 1.1

--- src/colegio/Colegio.java
+++ src/colegio/Colegio.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package colegio;
 
-/**
- *
- * @author Alumno
- */
-public class Colegio {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Colegio {
+ 
+    private int numeroDeEstudiantes;
+    private Boolean mixto;
+    private String nombre;
     
 }
