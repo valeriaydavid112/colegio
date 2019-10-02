98cfdfc8c1aabd5c7877514f1eafea43049b1a80
valeriaydavid112 <51164843+valeriaydavid112@users.noreply.github.com>
2019-10-02 13:00:22 -0500

    Version 1.2

--- src/colegio/Colegio.java
+++ src/colegio/Colegio.java
@@ -7,5 +7,37 @@ public class Colegio {
     private int numeroDeEstudiantes;
     private Boolean mixto;
     private String nombre;
+
+    public Colegio(int numeroDeEstudiantes, Boolean mixto, String nombre) {
+        this.numeroDeEstudiantes = numeroDeEstudiantes;
+        this.mixto = mixto;
+        this.nombre = nombre;
+    }
+
+    public int getNumeroDeEstudiantes() {
+        return numeroDeEstudiantes;
+    }
+
+    public void setNumeroDeEstudiantes(int numeroDeEstudiantes) {
+        this.numeroDeEstudiantes = numeroDeEstudiantes;
+    }
+
+    public Boolean getMixto() {
+        return mixto;
+    }
+
+    public void setMixto(Boolean mixto) {
+        this.mixto = mixto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
     
 }
---
+++ src/colegio/ColegioPublico.java
@@ -0,0 +1,12 @@
+
+package colegio;
+
+public class ColegioPublico extends Colegio{
+    
+    private int personalTecnico;
+    
+    public ColegioPublico(int numeroDeEstudiantes, Boolean mixto, String nombre) {
+        super(numeroDeEstudiantes, mixto, nombre);
+    }
+    
+}
