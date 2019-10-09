daafdb51c7cd60147a7169c1fa99741335081a7e
valeriaydavid112 <51164843+valeriaydavid112@users.noreply.github.com>
2019-10-09 10:57:28 -0500

    Update Colegio.java

--- src/colegio/Colegio.java
+++ src/colegio/Colegio.java
@@ -5,13 +5,17 @@ package colegio;
 public class Colegio {
  
     private int numeroDeEstudiantes;
+    private Date fechasDeMatriculas;
     private Boolean mixto;
     private String nombre;
-
-    public Colegio(int numeroDeEstudiantes, Boolean mixto, String nombre) {
+    private int numeroDeProfesores;
+ 
+    public Colegio(int numeroDeEstudiantes, Date fechaDeMatriculas, Boolean mixto, String nombre, int nuemroDeProdesores) {
         this.numeroDeEstudiantes = numeroDeEstudiantes;
+        this.fechaDeMatriculas=fechadDeMatriculas;
         this.mixto = mixto;
         this.nombre = nombre;
+        this.numeroDeProfesores=numeroDeProfesores;
     }
 
     public int getNumeroDeEstudiantes() {
@@ -22,6 +26,12 @@ public class Colegio {
         this.numeroDeEstudiantes = numeroDeEstudiantes;
     }
 
+    public Date getFechasDeInscripcion() {
+        return fechasDeMatriculas;
+    }
+    public void setFechasDeMatriculas(Date fechasDeMatriculas)  {
+        this.fechaDeMatriculas;
+    }
     public Boolean getMixto() {
         return mixto;
     }
@@ -37,7 +47,27 @@ public class Colegio {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    public int getNumeroDeProfesores() {
+        return numeroDeProfesores;
+    }
+    public void setNumeroDeProfesores(int numeroDeProfesores) {
+        this.numeroDeProfesores = numeroDeProfesores;
+    }
+ 
+     public void colegios(){
+        this.mixto += 10;
+    }
     
+    public void colegios(int nuevoMixto){
+        if(nuevoMixto<=this.mixto){
+            System.out.println("Mas numero de estudiantes ");
+        }else{
+            this.mixto = nuevomixto;
+            System.out.println("Nuevo mixto: " + String.valueOf(nuevaVelocidad) + " Educacion");            
+        }
+        
+    }
+
     
     
 }
